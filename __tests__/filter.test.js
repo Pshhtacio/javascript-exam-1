cc468bc8f57fc6b2b8fede4cdd7aad85d8f6392a
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-08 12:04:22 +0800

    (JavScript Exam 01) - added test for filterEvenNumbers

--- __tests__/filter.test.js
+++ __tests__/filter.test.js
@@ -1,13 +1,16 @@
 import { filterEvenNumbers, filterLengthWith4, filterStartWithA } from '../src/filter';
 
 describe('array filter test', () => {
-    // Please add test cases here
-    test('demo should return true', () => {
-    //    given
-    const numbers = [2, 3, 4];
-    //    when
-    const result = filterEvenNumbers(numbers);
-    //    then
-    expect(result).toBe[2, 4];
+    test('should return an array containing only even numbers [2, 4] when filterEvenNumbers is applied', () => {
+        const numbers = [1, 2, 3, 4];
+        const result = filterEvenNumbers(numbers);
+        expect(result).toBe[2, 4];
+    });
+
+    test('should return an empty array when filterEvenNumbers is applied', () => {
+        const numbers = [1, 3, 5];
+        const result = filterEvenNumbers(numbers);
+        const emptyArray = [];
+        expect(result).toEqual(emptyArray);
     });
 });
