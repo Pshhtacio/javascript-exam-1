fa4818b5f7c0ee27e41c5f9af5df2312e1e9d2de
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-08 17:03:55 +0800

    (JavScript Exam 01) - fix variable names

--- __tests__/filter.test.js
+++ __tests__/filter.test.js
@@ -16,26 +16,26 @@ describe('array filter test', () => {
 
     test('should return an array containing only words with the length of 4 when filterLengthWith4 is applied', () => {
         const words = ['Alpha', 'Beta', 'Charlie', 'Delta', 'Echo'];
-        const result = filterLengthWith4(numbers);
+        const result = filterLengthWith4(words);
         expect(result).toBe['Beta', 'Echo'];
     });
 
     test('should return an empty array when filterLengthWith4 is applied', () => {
         const words = ['Apple', 'Banana', 'Cat', 'Dog', 'Egg'];
-        const result = filterLengthWith4(numbers);
+        const result = filterLengthWith4(words);
         const emptyArray = [];
         expect(result).toEqual(emptyArray);
     });
 
     test('should return an array containing only words that starts with the letter a when filterStartWithA is applied', () => {
         const words = ['Alpha', 'ant', 'Beta', 'boy', 'Carcasses'];
-        const result = filterLengthWith4(numbers);
+        const result = filterLengthWith4(words);
         expect(result).toBe['Alpha', 'ant'];
     });
 
     test('should return an empty array when filterStartWithA is applied', () => {
         const words = ['Banana', 'Cat', 'Dog', 'Egg'];
-        const result = filterLengthWith4(numbers);
+        const result = filterLengthWith4(words);
         const emptyArray = [];
         expect(result).toEqual(emptyArray);
     });
